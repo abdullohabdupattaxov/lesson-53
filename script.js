178e8d996f0db44a999371fe4a2171e73b5582b0
abdullohabdupattaxov <abdulloxsteam928@gmail.com>
2025-08-14 13:09:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let bgColor = prompt("enter backround color:");
+
+
+let textColor = prompt("enter text color:");
+
+
+document.body.style.backgroundColor = bgColor;
+document.body.style.color = textColor;
